f48703083ee065d3a1cfdd4a7aba9f273db2daf8
Shubham <shubham@dreamkasper.com>
2026-08-16 19:42:09 +0530

    refactor: simplify password hashing middleware in user schema

--- src/models/User.js
+++ src/models/User.js
@@ -30,13 +30,12 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {

ecc58b6aa2b037f0522101a889fbf8ff010086c6
Shubham <shubham@dreamkasper.com>
2026-08-16 20:39:21 +0530

    feat: add transaction validation middleware for create, update, and retrieval operations

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -6,14 +6,37 @@ import {
   updateTransaction,
 } from "../controllers/transactionController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import {
+  createTransactionValidator,
+  getTransactionsValidator,
+  transactionIdValidator,
+  updateTransactionValidator,
+} from "../middlewares/transactionValidators.js";
+import validateRequest from "../middlewares/validateRequest.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/", createTransaction);
-router.get("/", getTransactions);
-router.put("/:id", updateTransaction);
-router.delete("/:id", deleteTransaction);
+router.post(
+  "/",
+  createTransactionValidator,
+  validateRequest,
+  createTransaction,
+);
+router.get("/", getTransactionsValidator, validateRequest, getTransactions);
+router.put(
+  "/:id",
+  transactionIdValidator,
+  updateTransactionValidator,
+  validateRequest,
+  updateTransaction,
+);
+router.delete(
+  "/:id",
+  transactionIdValidator,
+  validateRequest,
+  deleteTransaction,
+);
 
 export default router;
